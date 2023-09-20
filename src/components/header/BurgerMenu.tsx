e4cacc56f510dc7c3cac6c3ba69b44d36e3f0607
itsaliabdallah <itsaliabdallah@gmail.com>
2023-09-20 18:41:10 +0100

    add and implement burger menu component when on mobile.

---
+++ src/components/header/BurgerMenu.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+const BurgerMenu = ({ currentPage }: { currentPage: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      {isOpen ? (
+        <button onClick={toggleMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      ) : (
+        <button onClick={toggleMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      )}
+
+      <div
+        className={`absolute left-0 right-0 top-16 bg-background py-4 transition-opacity duration-300 ${
+          isOpen ? "opacity-100" : "pointer-events-none opacity-0"
+        }`}
+      >
+        <div className="flex flex-col items-center space-y-4">
+          <Link
+            className={`${currentPage === "" ? "border-b-2" : ""}`}
+            onClick={toggleMenu}
+            href={`/`}
+          >
+            Home
+          </Link>
+
+          <Link
+            className={`${currentPage === "about" ? "border-b-2" : ""}`}
+            onClick={toggleMenu}
+            href={`/about`}
+          >
+            About
+          </Link>
+
+          <Link
+            className={`${currentPage === "work" ? "border-b-2" : ""}`}
+            onClick={toggleMenu}
+            href={`/work`}
+          >
+            Work
+          </Link>
+
+          <Link
+            className={`${currentPage === "contact" ? "border-b-2" : ""}`}
+            onClick={toggleMenu}
+            href={`/contact`}
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BurgerMenu;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import { FunctionComponent } from "react";
+import BurgerMenu from "./BurgerMenu";
+import { usePathname } from "next/navigation";
 
 export const Header: FunctionComponent = () => {
+  const currentPage = usePathname();
   const linkStyle = "p-4";
 
   return (
     <header className="bg-black text-white flex justify-between items-center p-4 h-[77px] border-b-2 border-accent sticky top-0 z-10">
       <h1 className="font-black text-2xl">ALI ABDALLAH</h1>
 
-      <nav className="">
+      <nav className="hidden md:block">
         <Link className={linkStyle} href="">
           HOME
         </Link>
@@ -21,6 +26,10 @@ export const Header: FunctionComponent = () => {
         <Link className={linkStyle} href="">
           CONTACT
         </Link>
+      </nav>
+
+      <nav className="md:hidden">
+        <BurgerMenu currentPage={currentPage} />
       </nav>
     </header>
   );
